c5533642dbde96857d928532ea99fd36e22cad8d
devmuturi <muturinjonge09@gmail.com>
2025-07-22 12:54:02 +0300

    add the index for products

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,10 +1,14 @@
 class CartItemsController < ApplicationController
   def create
-  end
-
-  def update
+    product = Product.find(params[:product_id])
+    session[:cart] ||= {}
+    session[:cart][product.id.to_s] ||= 0
+    session[:cart][product.id.to_s] += 1
+    redirect_to products_path, notice: "#{product.name} added to cart."
   end
 
   def destroy
+    session[:cart].delete(params[:id])
+    redirect_to cart_path, notice: "Item removed."
   end
 end
