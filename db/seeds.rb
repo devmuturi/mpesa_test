0bd1b8714582238613ec8277ab8dc77c759cead4
devmuturi <muturinjonge09@gmail.com>
2025-07-22 12:47:05 +0300

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.create([
+  { name: "Ruby T-shirt", price: 1500 },
+  { name: "Rails Mug", price: 800 },
+  { name: "Sticker Pack", price: 200 }
+])
